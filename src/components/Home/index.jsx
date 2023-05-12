f2ded0b49db9639a97bf1a12408d214fb2fd93e5
chandrabhan357 <chandrabhan1366@hotmail.com>
2023-05-12 20:28:02 +0530

    New Components Added

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss';
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = [' ', 'C', 'h', 'a', 'n', 'd', 'r', 'a', 'b', 'h', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1><span className={letterClass}>H</span>
+        <span className={`${letterClass} _12`}>i,</span><br />
+        <span className={`${letterClass} _13`}>I</span>
+        <span className={`${letterClass} _14`}>'m</span>
+         
+        <AnimatedLetters letterClass={letterClass}
+        strArray={nameArray}
+        idx={15} />
+
+        <br />
+        <AnimatedLetters letterClass={letterClass}
+        strArray={jobArray}
+        idx={15} />
+        </h1>
+        <h2>Frontend Developer / HTML / CSS / JavaScript / React</h2>
+        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+      </div>
+        
+    </div>
+  )
+}
+
+export default Home
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,15 +1,16 @@
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoS from '../../assets/images/logo.svg'
 import { Link, NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faLinkedin,
+    faGithub,
+    faTwitter,
+  } from '@fortawesome/free-brands-svg-icons'
 import {
     faHome,
     faUser,
     faEnvelope,
-    faSuitcase,
-    faBars,
-    faClose,
   } from '@fortawesome/free-solid-svg-icons'
 
 
@@ -20,7 +21,8 @@ const Sidebar = () => {
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoS} alt="logo" />
-            <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+            
+            
         </Link>
         <nav>
             <NavLink exact="true" activeclasssname="active" to="/">
@@ -28,11 +30,48 @@ const Sidebar = () => {
 
             </NavLink>
 
-            <NavLink exact="true" activeclasssname="active" to="/">
+            <NavLink exact="true" activeclasssname="active" className="about-link" to="/about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
 
             </NavLink>
+
+            <NavLink exact="true" activeclasssname="active" className="contact-link"  to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+
+            </NavLink>
         </nav>
+        <ul>
+            <li>
+        <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/chandrabhan-b40a39249/'>
+        <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+            
+        </a>
+            </li>
+            <li>
+        <a target="_blank" rel='noreferrer' href='https://github.com/chandrabhan357'>
+        <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+            
+        </a>
+            </li>
+            <li>
+        <a target="_blank" rel='noreferrer' href='https://twitter.com/Chandra80311745'>
+        <FontAwesomeIcon
+              icon={faTwitter}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+            
+        </a>
+            </li>
+        </ul>
 
     </div>
   )
